283b37b9e8c1721cc769d9d3864dfa8d066cc8c9
Shivanshi Sharma <neelusharma895764@gmail.com>
2026-04-20 16:09:14 +0530

    Connected frontend to deployed backend

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API = "http://127.0.0.1:8000"
+API = "https://ai-code-assistant-f1lw.onrender.com"
 
 st.set_page_config(page_title="AI Code Assistant", layout="wide")
 
